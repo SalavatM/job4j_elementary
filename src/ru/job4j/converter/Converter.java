3efadb95e154540993b47702a318ac034ba7e5d4
SalavatM <techprosm@yandex.ru>
2020-09-09 13:27:02 +0300

    0. Что такое тестирование [#357744]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,5 +1,8 @@
 package ru.job4j.converter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Converter {
 
     public static float rubleToEuro(float value) {
@@ -8,14 +11,21 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
+        //округление до центов
+        float rsl = new BigDecimal(value / 60).setScale(2, RoundingMode.DOWN).floatValue();
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+
+        expected = 2.33f;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("140 rubles are 2.33 dollar. Test result : " + passed);
     }
 }
