1a6c09aad70386cda5800f598921d0f1869fd094
SalavatM <techprosm@yandex.ru>
2020-09-09 13:58:28 +0300

    1. Модульные тесты. [#357745]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -5,27 +5,27 @@ import java.math.RoundingMode;
 
 public class Converter {
 
-    public static float rubleToEuro(float value) {
-        float rsl = value / 70;
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
         return rsl;
     }
 
-    public static float rubleToDollar(float value) {
-        //округление до центов
-        float rsl = new BigDecimal(value / 60).setScale(2, RoundingMode.DOWN).floatValue();
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float in = 140;
-        float expected = 2;
-        float out = Converter.rubleToEuro(in);
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
 
-        expected = 2.33f;
+        in = 180;
+        expected = 3;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
-        System.out.println("140 rubles are 2.33 dollar. Test result : " + passed);
+        System.out.println("180 rubles are 3 dollar. Test result : " + passed);
     }
 }
